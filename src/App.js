18c1ad48e94d61ba0c823d96157edf2710d5e89c
Abdul Moiz <221028@students.au.edu.pk>
2023-04-15 11:51:43 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {FaFacebook} from 'react-icons/fa'
 import {FaGoogle} from 'react-icons/fa'
 import {FaInstagram} from 'react-icons/fa'
+import "./app.css";
 
 
 const App=()=>{
